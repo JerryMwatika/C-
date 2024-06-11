8ffef679d94f7e7d6814ff47b76cbb5f0814cc50
JerryMwatika <jerry.mwatika@strathmore.edu>
2024-06-11 20:00:54 +0300

    commit

---
+++ area.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //declare variables
+    int length,width,area;
+
+    //initialize variables
+    length = 8;
+    width =5 ;
+    area = length*width;
+
+    //access the variables
+
+    cout<<"the area of the rectangle ="+area;
+}
